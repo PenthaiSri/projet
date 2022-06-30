9d6ba87f80b5e3f2b819c4c985e8cb1a24916263
Penthai Sriruang <penthai.sriruang@gmail.com>
2022-06-30 20:16:51 +0200

    maj

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -1,51 +1,42 @@
 <!DOCTYPE html>
 
 <html>
-
 <head>
     <title>Liste des modules</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{url('css/list.css')}}" rel="stylesheet" type="text/css">
-
-    <style>
-    body {
-        background-color: #caf52d;
-    }
-
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #333;
-    }
-
-    li {
-        float: center;
-    }
-
-    li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-
-    li a:hover {
-        background-color: #111;
-    }
-    </style>
 </head>
 
 <body>
-    <h1>Hello world !</h1>
     <ul>
         <li><a href="#home">Acceuil</a></li>
         <li><a href="#news">Relevés</a></li>
         <li><a href="#contact">Météo</a></li>
     </ul>
+
+    <div id="panel" class="module-state">
+        <h1 class="text-panel">Les modules sont opérationnels</h1>
+    </div>
+    <div id="Module1" class="module-list">
+        <div id="number" class="number">
+        </div>
+        <div id="plant_name" class="name">
+            <h2>Nom de la plante</h2>
+        </div>
+        <div class="air">
+        </div>
+        <div class="humidity">
+            <h2>Humidité du sol</h2>
+            <hr>
+        </div>
+    </div>
+    <div id="Module2" class="module-list">
+        <p>salut</p>
+    </div>
+    <div id="Module3" class="module-list">
+        <p>salut</p>
+    </div>
 </body>
 
 </html>
