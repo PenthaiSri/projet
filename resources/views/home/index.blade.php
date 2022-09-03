6c1a1e938b394fce63b9d618e95acb270828bd2b
Florent DAGNOT <florent.dagnot@laposte.net>
2022-09-03 10:09:25 +0200

    Ajout accueil

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login page</title>
+    <link rel="stylesheet" type="text/css" href="{{url('css/home.css')}}">
+    <style>
+    body {
+        background-image: url('/projet/resources/images/background.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="resume-up">
+        <h1 style="color:white; text-align:right">Tous les modules sont au vert</h1>
+        <img src="/projet/resources/images/Green-check-mark-on-transparent-background-PNG.png"
+        width="200"
+        height="200"
+        title="L'ensemble du système fonctionne normalement, pensez à vérifier le niveau d'eau !"
+        align="left">
+    </div>
+    <div class="resume-down">
+        <div class="log-modules">
+            <h2 style="color:white;">Messages et activités des modules</h2>
+            <input id="email" type="text" style="text-align:center;" placeholder="Identifiant" name="email">
+        </div>
+        <div class="meteo">
+            <input id="email" type="text" style="text-align:center;" placeholder="Identifiant" name="email">
+        </div>     
+    </div>
+</body>
+
+
+</html>
