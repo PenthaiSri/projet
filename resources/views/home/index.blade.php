82549c916960c95fbee28b5ba1f1dc18eb78cd31
Florent DAGNOT <florent.dagnot@laposte.net>
2022-09-04 15:51:36 +0200

    Front Accueil

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -16,20 +16,39 @@
 
 <body>
     <div class="resume-up">
-        <h1 style="color:white; text-align:right">Tous les modules sont au vert</h1>
         <img src="/projet/resources/images/Green-check-mark-on-transparent-background-PNG.png"
-        width="200"
-        height="200"
+        width="120"
+        height="120"
         title="L'ensemble du système fonctionne normalement, pensez à vérifier le niveau d'eau !"
         align="left">
+        <div class="resume-in" style="text-align:center">
+            <h1 style="color:white;">Tous les modules sont au vert</h1>
+            <button class="bouton" type="button" style="color:white; font-weight: bold;">Accéder aux modules</button>
+        </div>
     </div>
     <div class="resume-down">
         <div class="log-modules">
             <h2 style="color:white;">Messages et activités des modules</h2>
-            <input id="email" type="text" style="text-align:center;" placeholder="Identifiant" name="email">
+            <div class="txt-log">
+                <p style="vertical-align: top;">Début des logs</p>
+            </div>
         </div>
         <div class="meteo">
-            <input id="email" type="text" style="text-align:center;" placeholder="Identifiant" name="email">
+            <div>
+                <h2 style="color:white;">Dimanche 24 Juillet</h2>
+                <h3 style="color:white;">17h47 à Paris</h3>
+                <div>
+                    <img src="/projet/resources/images/Sun-And-Cloud-PNG-Transparent.png"
+                    width="100"
+                    height="100"
+                    title="L'ensemble du système fonctionne normalement, pensez à vérifier le niveau d'eau !"
+                    align="left">
+                    <div class="temperature">
+                        <h1 style="color:#ADD8E6;">23°C</h1>
+                        <p style="color:white;">Partiellement nuageux</p>
+                    </div>
+                </div>
+            </div>
         </div>     
     </div>
 </body>
